deaa79fc56facc939a01c57a55b578a7f1b5aa59
StefaniaDev <stefania.zinno@unina.it>
2021-02-12 21:02:10 +0100

    Initial Commit

---
+++ Shared/ListCoreScratchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListCoreScratchApp.swift
+//  Shared
+//
+//  Created by Stefania Zinno on 12/02/21.
+//
+
+import SwiftUI
+
+@main
+struct ListCoreScratchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
